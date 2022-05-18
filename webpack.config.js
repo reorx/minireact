7e6c2537c34a498360a7a3d53513df88ba92468d
Reorx <novoreorx@gmail.com>
2022-05-18 15:12:37 +0800

    use one single webpack.config.js instead of multiple files

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
 const webpack = require('webpack')
-const CopyPlugin = require('copy-webpack-plugin')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const path = require('path')
-const rootDir = path.join(__dirname, '..')
+const rootDir = __dirname
 const srcDir = path.join(rootDir, 'src')
 const destDir = path.join(rootDir, 'build')
 
-module.exports = {
+const isDevelopment = process.env.NODE_ENV !== 'production'
+const useAnalyze = !!process.env.WEBPACK_USE_ANALYZE
+
+
+let config = {
   entry: {
     index: path.join(srcDir, 'index.tsx'),
   },
   output: {
-    // path: path.join(destDir, 'static/js'),
     path: destDir,
     filename: '[name].bundle.js',
   },
@@ -38,15 +40,46 @@ module.exports = {
     plugins: [new TsconfigPathsPlugin()],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.APP_ENV': JSON.stringify(process.env.APP_ENV),
+    }),
     new HtmlWebpackPlugin({
       inject: true,
       template: path.join(rootDir, 'public/index.html'),
     })
-    // new CopyPlugin({
-    //   patterns: [{
-    //     from: path.join(rootDir, 'public'),
-    //     to: destDir
-    //   }],
-    // }),
   ],
 }
+
+
+
+if (useAnalyze) {
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+  config.mode = 'development'
+  config.plugins.push(
+    new BundleAnalyzerPlugin({
+      analyzerPort: 18888,
+    })
+  )
+} else if (isDevelopment) {
+  Object.assign(config, {
+    devtool: 'cheap-module-source-map',
+    mode: 'development',
+    devServer: {
+      host: '127.0.0.1',
+      port: 3000,
+      hot: true,
+      historyApiFallback: true,
+      // allowedHosts: [],
+    },
+  })
+} else {
+  Object.assign(config, {
+    mode: 'production',
+    optimization: {
+      minimize: true,
+    }
+  })
+}
+
+module.exports = config
